0de4a1000f8abcead0a0f6a159cb2110fe441dd7
calfarera <calfareraagil@gmail.com>
2022-05-10 17:29:47 +0700

    Section 2

---
+++ API/Data/Migrations/20220510100425_InitialCreate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Products");
+        }
+    }
+}
