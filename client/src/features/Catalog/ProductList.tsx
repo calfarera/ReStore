fdb2b6f762e9d8fefb6f808ec280acad40d124e6
calfarera <calfareraagil@gmail.com>
2022-05-17 15:36:58 +0700

    Section 3

---
+++ client/src/features/Catalog/ProductList.tsx
@@ -0,0 +1,21 @@
+import { Grid } from '@mui/material'
+import { Product } from "../../app/Models/Product";
+import ProductCard from './ProductCard';
+
+interface Props {
+    products: Product[];
+}
+
+export default function ProductList({products}: Props) {
+  return (
+    <div>
+        <Grid container spacing={4}  >
+            {products.map(product => (           
+                <Grid item xs={3} key={product.id} >
+                  <ProductCard product={product} />
+                </Grid>
+            ))}
+        </Grid>
+    </div>
+  )
+}
